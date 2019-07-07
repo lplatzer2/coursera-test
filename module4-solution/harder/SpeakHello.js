35894ad6d37625ffcef138e81b99a692addf029c
Laura Platzer <lplatzer2@gmail.com>
2019-07-07 17:07:25 -0400

    Module 4 solution

---
+++ module4-solution/harder/SpeakHello.js
@@ -0,0 +1,13 @@
+(function speakHello(argument) {
+	var helloSpeaker={};
+	var speakWord = "Hello";
+
+
+	helloSpeaker.speak=function(name) {
+	  console.log(speakWord + " " + name);
+	}
+
+	window.helloSpeaker=helloSpeaker;
+
+	
+})(window);
